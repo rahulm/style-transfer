c8d379fd335594fc90346285fec28679ddb7868c
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-10-31 14:31:26 -0700

    changed default variation loss to 1

--- style.py
+++ style.py
@@ -193,7 +193,7 @@ def run():
       "weights" : {
         "content" : 0,
         "style" : 1,
-        "variation" : 0
+        "variation" : 1
       }
       
       # "weights" : {
@@ -218,7 +218,7 @@ def run():
       "weights" : {
         "content" : 0,
         "style" : 1,
-        "variation" : 0
+        "variation" : 1
       }
       
       # "weights" : {
