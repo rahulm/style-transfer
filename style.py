a599f6f459e78255a6f77e946dbbe55f23b998a4
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-10-30 23:35:57 -0700

    removed some old comments

--- style.py
+++ style.py
@@ -4,34 +4,6 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 
-# import tensorflow as tf
-
-# print("Testing tensorflow.")
-
-# mnist = tf.keras.datasets.mnist
-
-# (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
-# xTrain, xTest = xTrain / 255.0, xTest / 255.0
-
-# model = tf.keras.models.Sequential([
-  # tf.keras.layers.Flatten(input_shape=(28, 28)),
-  # tf.keras.layers.Dense(128, activation='relu'),
-  # tf.keras.layers.Dropout(0.2),
-  # tf.keras.layers.Dense(10, activation='softmax')
-# ])
-
-# model.compile(optimizer='adam',
-              # loss='sparse_categorical_crossentropy',
-              # metrics=['accuracy'])
-
-
-# model.fit(xTrain, yTrain, epochs=5)
-# model.evaluate(xTest,  yTest, verbose=2)
-
-# print("Done")
-
-# maybe try ResNet-18 or ResNet-34 because they are smaller in size than the VGGs? (due to hardware limitations)
-
 
 def readArgs():
   parser = argparse.ArgumentParser(
